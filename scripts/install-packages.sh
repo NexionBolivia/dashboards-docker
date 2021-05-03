6ea5b3ed4a9feae368e6e111d948b8ecbe3f6758
Rodrigo Caballero Hurtado <rodrigo.caballero@nexion.com.bo>
2021-05-03 09:50:58 -0400

    Adding new package

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -10,4 +10,6 @@ sudo su - -c "R -e \"install.packages('httr')\"" &&
 sudo su - -c "R -e \"install.packages('haven')\"" &&
 sudo su - -c "R -e \"install.packages('rworldmap')\"" &&
 sudo su - -c "R -e \"install.packages('leaflet')\"" &&
-sudo su - -c "R -e \"install.packages('pps')\""
+sudo su - -c "R -e \"install.packages('pps')\"" &&
+sudo su - -c "R -e \"install.packages('jsonlite')\"" &&
+sudo su - -c "R -e \"install.packages('readr')\""
